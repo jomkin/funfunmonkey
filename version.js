4be873dd99ab00c0acb707a1621df657763b6fe5
jomkin <jomkin@qq.com>
2014-10-10 23:33:09 +0800

    Update version.js

--- version.js
+++ version.js
@@ -6,3 +6,25 @@ function maybe(){
 }
 
 
+rateFunction()
+function rateFunction(){
+       
+var cfg = {
+        usesUntilPrompt: 8,
+        displayAppName: "My Super App",
+        customLocale: {
+            title: "Rate %@",
+            message: "If you enjoy using %@, would you mind taking a moment to rate it? It won’t take more than a minute. Thanks for your support!",
+            buttonLabels: ["No, Thanks", "Remind Me Later", "Rate It Now",]
+        },
+        appStoreAppURL: {
+            ios: 'itms-apps://itunes.apple.com/ru/app/id736199575?l=en&mt=8'
+        }
+    };
+
+    navigator.apprate.setup(cfg);
+    navigator.apprate.promptForRating();
+   
+}
+
+
